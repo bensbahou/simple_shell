4c49ac9f21d58dc864092853969d1a281d6459b6
bensbahou <younesbensbahou@gmail.com>
2023-05-24 11:52:31 -0700

    simple promt with loop

---
+++ shell.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point of the shell
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char *prompt = ">> ";
+	char *read_line = NULL;
+	size_t n = 0;
+
+	while (1)
+	{
+		printf("%s", prompt);
+		getline(&read_line, &n, stdin);
+		printf("%s\n", read_line);
+	}
+	free(read_line);
+	return (0);
+}
